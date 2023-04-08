2d3dd46ea2601728f622dce8f84f041537d4a409
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-04-08 17:39:32 +0530

    designed till about us

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-function FooterOverlay() {
-  return (
-    <div>FooterOverlay</div>
-  )
-}
+import './FooterOverlay.css';
+
+const FooterOverlay = () => (
+  <div className="app__footerOverlay">
+    <div className="app__footerOverlay-black" />
+    <div className="app__footerOverlay-img app__bg" />
+  </div>
+);
 
 export default FooterOverlay;
